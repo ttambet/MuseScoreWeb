2aafae565c53b177cfc910214b4d9db86c955600
Timo Tambet <ttambet@gmail.com>
2024-07-02 11:33:12 +0300

    Add config file

---
+++ libmscore/config.h
@@ -0,0 +1,11 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+#define MUSESCORE_NAME_VERSION "MuseScore 3.3.0"  // Replace with the actual name and version
+#define VERSION "3.3.0"  // Replace with the actual version of MuseScore
+#define BUILD_NUMBER "3543170"  // Replace with the actual build number
+
+#define INSTALL_NAME "mscore-3.6/"
+#define INSTPREFIX "/usr/local"
+
+#endif // CONFIG_H
