d2e26b500231f2c2fbda2aa21322833c1eb90a85
Timo Tambet <ttambet@gmail.com>
2024-07-04 10:58:57 +0300

    Add missing files

---
+++ mscore/scoreeditorwindow.h
@@ -0,0 +1,14 @@
+#ifndef SCOREEDITORWINDOW_H
+#define SCOREEDITORWINDOW_H
+
+#include <QtGui>
+
+class ScoreEditorWindow : public QMainWindow
+{
+public:
+    ScoreEditorWindow();
+
+private:
+};
+
+#endif // SCOREEDITORWINDOW_H
